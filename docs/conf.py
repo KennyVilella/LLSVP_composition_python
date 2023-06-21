636aefe1bb1862b3d6dbf2969f73718c7beb333d
KennyVilella <89772195+KennyVilella@users.noreply.github.com>
2023-06-21 13:39:08 +0900

    Add multi-versioned documentation (#22)
    # Description
    - Added multi-versioned documentation
    - Changed doc deployment method

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,17 @@ author = "Kenny Vilella"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx_multiversion",
 ]
 
 templates_path = ["_templates"]
+html_sidebars = {
+    '**': [
+        'versioning.html',
+    ],
+}
 html_theme = "sphinx_rtd_theme"
+
+# Whitelist pattern for tags and branches used for multi-versioning
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
+smv_branch_whitelist = r'^.*$'
