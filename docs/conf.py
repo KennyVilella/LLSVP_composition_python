908aa61d61e5d54822c50b00d0e0544972675760
Kenny Vilella <kennyvilella@gmail.com>
2023-06-21 12:52:47 +0900

    [added] multi-version documentation

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,17 @@ author = "Kenny Vilella"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx_multiversion",
 ]
 
 templates_path = ["_templates"]
+html_sidebars = {
+    '**': [
+        'versioning.html',
+    ],
+}
 html_theme = "sphinx_rtd_theme"
+
+# Whitelist pattern for tags and branches used for multiversioning
+smv_tag_whitelist = r'^v\d+\.\d+$'
+smv_branch_whitelist = r'^(main)$'
