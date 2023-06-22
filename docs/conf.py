70a6b9b6e0fe4704b5cbb1ad8ad700f8e0ee17fa
Kenny Vilella <kennyvilella@gmail.com>
2023-06-22 09:42:43 +0900

    [added] import of files in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+import src._spin_configuration
+import src._mineral_composition
+import src._seismic_anomalies
 
 project = "LLSVP composition calculator"
 version = "0.0.0"
