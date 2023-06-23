6e2fef2046e8720af3b213474047d288892b34db
Kenny Vilella <kennyvilella@gmail.com>
2023-06-23 14:53:05 +0900

    [changed] csv module to builtin functions

--- src/_mineral_composition.py
+++ src/_mineral_composition.py
@@ -39,7 +39,6 @@ Copyright, 2023,  Vilella Kenny.
 import numpy as np
 from math import isclose
 import os
-import csv
 import random
 import scipy.optimize
 from ._eos_implementation import _EOS_fp, _EOS_bm, _EOS_capv
@@ -124,29 +123,30 @@ def _calc_mineral_composition(self, spin_config: np.ndarray, P_table:np.ndarray)
             ### A file already exists ###
             print("File detected")
             print("Resuming the simulation")
-            with open(filename, newline='') as csvfile:
-                data = csv.reader(csvfile, delimiter=' ')
+            with open(filename, newline='') as f:
+                data = f.readlines()
                 for row in data:
                     # Calculating the corresponding indices
-                    jj = round((float(row[1]) - self.p_capv_min) / self.delta_capv)
-                    kk = round((float(row[2]) - self.p_bm_min) / self.delta_bm)
+                    processed_row = [float(s) for s in row.split()]
+                    jj = round((processed_row[1] - self.p_capv_min) / self.delta_capv)
+                    kk = round((processed_row[2] - self.p_bm_min) / self.delta_bm)
                     ll = round(
-                        (float(row[3]) - self.iron_content_min) /
+                        (processed_row[3] - self.iron_content_min) /
                         self.delta_iron_content
                     )
                     mm = round(
-                        (float(row[4]) - self.al_content_min) / self.delta_al
+                        (processed_row[4] - self.al_content_min) / self.delta_al
                     )
                     nn = round(
-                        (float(row[5]) - self.ratio_fe_bm_min) / self.delta_ratio_fe
+                        (processed_row[5] - self.ratio_fe_bm_min) / self.delta_ratio_fe
                     )
 
                     # Loading the data
-                    x_feo_bm_ii[jj, kk, ll, mm, nn] = row[6]
-                    x_feo_fp_ii[jj, kk, ll, mm, nn] = row[7]
-                    x_alo2_bm_ii[jj, kk, ll, mm, nn] = row[8]
-                    rho_bm_ii[jj, kk, ll, mm, nn] = row[9]
-                    rho_fp_ii[jj, kk, ll, mm, nn] = row[10]
+                    x_feo_bm_ii[jj, kk, ll, mm, nn] = processed_row[6]
+                    x_feo_fp_ii[jj, kk, ll, mm, nn] = processed_row[7]
+                    x_alo2_bm_ii[jj, kk, ll, mm, nn] = processed_row[8]
+                    rho_bm_ii[jj, kk, ll, mm, nn] = processed_row[9]
+                    rho_fp_ii[jj, kk, ll, mm, nn] = processed_row[10]
 
             # Initializing the loop
             jj_start = jj
@@ -215,12 +215,13 @@ def _calc_mineral_composition(self, spin_config: np.ndarray, P_table:np.ndarray)
                             rho_fp_ii[jj, kk, ll, mm, nn] = x_result[4]
 
                             # Writting the data
-                            with open(filename, 'a', newline='') as csvfile:
-                                data_writer = csv.writer(csvfile, delimiter=' ')
-                                data_writer.writerow([
-                                    dT, p_capv, p_bm, feo, al, ratio_fe, x_result[0],
-                                    x_result[1], x_result[2], x_result[3], x_result[4]
-                                ])
+                            with open(filename, 'a', newline='') as f:
+                                f.write(
+                                    f"{dT:.0f}\t{p_capv:.2f}\t{p_bm:.2f}\t{feo:.3f}\t" +
+                                    f"{al:.3f}\t{ratio_fe:.1f}\t{x_result[0]:.3f}\t" +
+                                    f"{x_result[1]:.3f}\t{x_result[2]:.3f}\t" +
+                                    f"{x_result[3]:.0f}\t{x_result[4]:.0f}\n"
+                                )
                             p_capv_ii[jj, kk, ll, mm, nn] = p_capv
                             p_bm_ii[jj, kk, ll, mm, nn] = p_bm
                             feo_ii[jj, kk, ll, mm, nn] = feo
@@ -480,7 +481,7 @@ def _solve_with_fp(
     To address this challenge, the function first attempts to find a solution using the
     provided starting conditions. If a solution cannot be found, the starting conditions
     are randomly sampled within predetermined ranges until a solution is obtained or the
-    number of attempts exceeds 1000. If a solution cannot be found under these
+    number of attempts exceeds 10. If a solution cannot be found under these
     conditions, a value of 0.0 is returned for each property.
 
     If finding a solution remains difficult, the user may manually increase the maximum
@@ -516,7 +517,7 @@ def _solve_with_fp(
     # Initialization
     n_iter = 0
 
-    while (n_iter < 100):
+    while (n_iter < 10):
         ### Solution has not yet been found ###
         try:
             ### Solution has been found ###
@@ -767,7 +768,7 @@ def _solve_without_fp(
     To address this challenge, the function first attempts to find a solution using the
     provided starting conditions. If a solution cannot be found, the starting conditions
     are randomly sampled within predetermined ranges until a solution is obtained or the
-    number of attempts exceeds 1000. If a solution cannot be found under these
+    number of attempts exceeds 10. If a solution cannot be found under these
     conditions, a value of 0.0 is returned for each property.
 
     If finding a solution remains difficult, the user may manually increase the maximum
@@ -798,7 +799,7 @@ def _solve_without_fp(
     # Initialization
     n_iter = 0
 
-    while (n_iter < 100):
+    while (n_iter < 10):
         ### Solution has not yet been found ###
         try:
             ### Solution has been found ###
